c93c6b11065103ff48714f778fa3889cfc7a334e
Niclas Lund <niclas.lund@telia.com>
2021-12-05 17:32:22 +0100

    typo

--- 2021/day_04.py
+++ 2021/day_04.py
@@ -1,7 +1,7 @@
 """
     Advent of Code
     2021 Day 4
-    Giant Squis
+    Giant Squid
     
     You're already almost 1.5km (almost a mile) below the surface of the ocean, already so deep that you can't see any sunlight. 
     What you can see, however, is a giant squid that has attached itself to the outside of your submarine.
