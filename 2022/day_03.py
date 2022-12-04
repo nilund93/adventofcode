84c9431c387f7ba37cb4d7d7af6f2ac2176f523a
Niclas L <nilund@kth.se>
2022-12-04 23:25:33 +0100

    day 3 boilerplate

---
+++ 2022/day_03.py
@@ -0,0 +1,16 @@
+def import_puzzle():
+    with open("2022/puzzle_input/03.txt", "rt") as f:
+        return [line.strip("\n").split(" ") for line in f.readlines()]
+        
+def import_example():
+    with open("2022/example_input/03.txt", "rt") as f:
+        return [line.strip("\n").split(" ") for line in f.readlines()]
+    
+def part_one():
+    pass
+
+def part_two():
+    pass
+
+if __name__ == "__main__":
+    pass
