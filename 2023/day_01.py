ae7007e280acd268e979d5ea83caa9e224fc6369
Niclas L <nilund@kth.se>
2023-12-02 16:59:30 +0100

    formatting

--- 2023/day_01.py
+++ 2023/day_01.py
@@ -1,4 +1,4 @@
-#On each line, the calibration value can be found by 
+# On each line, the calibration value can be found by 
 # combining the first digit and the last digit (in that order) 
 # to form a single two-digit number.
 # What is the sum of all of the calibration values?
